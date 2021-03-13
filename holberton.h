696662ff7c2ead0b31c3967702d9933987c0e6bd
Nels <nelsfichera@gmail.com>
2021-03-13 14:21:15 -0500

    Add typedef

--- holberton.h
+++ holberton.h
@@ -4,4 +4,10 @@
 #include <stdarg.h>		/* vargs */
 #include <unistd.h> 		/* write */
 int _printf(const char *format, ...);
+ typedef struct functions
+{
+	 char letter;
+	 void(*f)();
+
+} argument_t;
 #endif	/* HOLBERTON_H */
