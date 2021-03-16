a3c2f22c64add16de840256ff8376a246fb93ffc
keysmusician <justin.masayda@gmail.com>
2021-03-16 20:12:40 +0000

    Exclude reverse text

--- text.c
+++ text.c
@@ -83,6 +83,7 @@ char *format_string(va_list argument)
 		new_string[i] = string[i];
 	return (new_string);
 }
+#if 0
 /**
 * format_reverse - Prints an argument string in reverse
 * @argument: the argument
@@ -108,3 +109,4 @@ char *format_reverse(va_list argument)
 	reverse[x] = '\0';
 	return (reverse);
 }
+#endif
